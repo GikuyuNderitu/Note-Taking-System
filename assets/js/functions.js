889d24f1c2e270c0064981cb1cc1ea0542a68cfa
GikuyuNderitu <gikuyunderitu@gmail.com>
2016-07-25 03:29:41 -0400

    Add js assets

---
+++ assets/js/functions.js
@@ -0,0 +1,34 @@
+function closeNotes(){
+   $(".note-section").toggleClass();
+}
+
+$(document).ready(function(){
+   var currentContext = $(".active");
+
+   $(".note-head a:first-child").click(function(){
+	  $(".note-section").toggleClass("closeNotes");
+	  $(".note-main").toggleClass("closeNotes");
+	  $(".tab").toggleClass("closeNotes");
+   });
+
+   $(".note-toggle a:first-child").click(function(){
+	  $(".note-section").toggleClass("closeNotes");
+	  $(".note-main").toggleClass("closeNotes");
+	  $(".tab").toggleClass("closeNotes");
+   });
+
+   $("#instructions-toggle").click(function(){
+	  $(".instructions-section").toggleClass("hideDiv");
+   });
+
+   $("#flag-toggle").click(function(){
+	  $(".flag-section").toggleClass("hideDiv");
+   });
+
+   $(".tab").click(function(){
+	  currentContext.toggleClass("active");
+	  currentContext = $(this);
+	  currentContext.toggleClass("active");
+
+   });
+});
